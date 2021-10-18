e6eb5b78c1c0544666d2b8b81663d0f4beb78691
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-18 17:54:22 +0200

    put NameBox component in a separate file, added task description
    Task description states: "The following components are created: Player,  GameAdmin (holiding two instances of Player component and  the panel with input fields). Place each component in a separate file."
    
    So I'm putting NameBox component in a separate file.

--- lab2/src/GameAdmin.tsx
+++ lab2/src/GameAdmin.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Player from "./Player";
+import NameBox from "./NameBox";
 
 interface IPlayerData {
   playerNo: string;
@@ -7,26 +8,6 @@ interface IPlayerData {
   playCount: number;
   isPlaying: boolean;
 }
-
-const NameBox = (props: { playerData: any; changeName: any }) => (
-  <div>
-    <div className="row m-3 mb-2">
-      <div className="col-auto ">
-        <label htmlFor="name">
-          Set name of <b>{props.playerData.playerNo}</b>:
-        </label>
-      </div>
-      <div className="col">
-        <input
-          id="namebox"
-          className="w-100"
-          value={props.playerData.playerName}
-          onChange={(e) => props.changeName(props.playerData, e.target.value)}
-        />
-      </div>
-    </div>
-  </div>
-);
 
 const GameAdmin = () => {
   //initial state of players
@@ -51,14 +32,12 @@ const GameAdmin = () => {
     setPlayerDatas(
       playerDatas.map((PlayerData) => {
         if (PlayerData === data) {
-          console.log(PlayerData);
           return {
             ...PlayerData,
             isPlaying: true,
             playCount: PlayerData.playCount + 1,
           };
         } else {
-          console.log(PlayerData);
           return {
             ...PlayerData,
             isPlaying: false,
