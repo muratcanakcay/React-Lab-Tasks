924cfe52364506444a35600bb36c2973819b12c8
Muratcan Akcay <muratcanakcay@gmail.com>
2021-11-17 01:37:27 +0100

    checkerboard with variable dimensions implemented

--- lab6/src/components/Board.tsx
+++ lab6/src/components/Board.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
 
+const noRows = 12
+const noColumns = 12
 
 const rows = () => {
     return (
-        Array.from(new Array(8)).map((_, i) => (
+        Array.from(new Array(noRows)).map((_, r) => (
             <tr>
-                {cols()}
+                {cols(r)}
             </tr>
         )
         ))
 }
 
-const cols = () => {
+const cols = (r: number) => {
     return (
-        Array.from(new Array(8)).map((_, i) => (
-            <td className={i % 2 ? "light" : "dark"}></td>
+        Array.from(new Array(noColumns)).map((_, c) => (
+            <td className={(c + r) % 2 ? "light" : "dark"}></td>
         )
         ))
 }
